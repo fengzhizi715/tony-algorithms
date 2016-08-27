f2c597361b00f4f4c3e874908d4e89cbc45df324
Tony Shen <fengzhizi715@126.com>
2016-08-27 11:58:56 +0800

    增加非递归实现的遍历

--- src/tree/Tree.java
+++ src/tree/Tree.java
@@ -1,5 +1,7 @@
 package tree;
 
+import java.util.Stack;
+
 /**
  * Created by tony on 16/8/25.
  */
@@ -11,9 +13,14 @@ public class Tree<T> {
         this.root = root;
     }
 
-    /*二叉树的节点*/
+    /**
+     * 二叉树的节点
+     * @param <T>
+     */
     private static class Node<T> {
+
         T element;  //节点的值
+
         Node<T> lchild, rchild; //左右孩子节点
 
         public Node(T element) {
@@ -27,7 +34,10 @@ public class Tree<T> {
         }
     }
 
-    /*先序遍历*/
+    /**
+     * 先序遍历
+     * @param root
+     */
     public void preorder(Node<T> root) {
         if(root != null) {
             System.out.println(root.element);
@@ -36,7 +46,10 @@ public class Tree<T> {
         }
     }
 
-    /** 递归实现中序遍历 */
+    /**
+     * 递归实现中序遍历
+     * @param root
+     */
     public void inorder(Node<T> root) {
         if (root != null) {
             inorder(root.lchild);
@@ -45,8 +58,11 @@ public class Tree<T> {
         }
     }
 
-    /** 递归实现后序遍历 */
-    public  void postorder(Node root) {
+    /**
+     * 递归实现后序遍历
+     * @param root
+     */
+    public void postorder(Node<T> root) {
         if (root != null) {
             postorder(root.lchild);
             postorder(root.rchild);
@@ -54,15 +70,61 @@ public class Tree<T> {
         }
     }
 
+    /**
+     * 非递归实现前序遍历
+     * @param root
+     */
+    public void iterativePreorder(Node<T> root) {
+        Stack<Node> stack = new Stack<Node>();
+        if (root != null) {
+            stack.push(root);
+            while (!stack.empty()) {
+                root = stack.pop();
+                System.out.println(root.element);
+                if (root.rchild != null)
+                    stack.push(root.rchild);
+                if (root.lchild != null)
+                    stack.push(root.lchild);
+            }
+        }
+    }
+
+    /**
+     * 非递归实现中序遍历
+     * @param root
+     */
+    public void iterativeInorder(Node<T> root) {
+        Stack<Node> stack = new Stack<Node>();
+        while (root != null) {
+            while (root != null) {
+                if (root.rchild != null)
+                    stack.push(root.rchild);// 当前节点右子入栈
+                stack.push(root);// 当前节点入栈
+                root = root.lchild;
+            }
+            root = stack.pop();
+            while (!stack.empty() && root.rchild == null) {
+                System.out.println(root.element);
+                root = stack.pop();
+            }
+            System.out.println(root.element);
+            if (!stack.empty())
+                root = stack.pop();
+            else
+                root = null;
+        }
+    }
+
     /*main方法用于测试*/
     public static void main(String[] args) {
-        Node<String> d = new Node<String>("D");
+        Node<String> f = new Node<String>("F");
+        Node<String> d = new Node<String>("D",f,null);
         Node<String> e = new Node<String>("E");
         Node<String> lchild = new Node<String>("B", null, d);
         Node<String> rchild = new Node<String>("C", e, null);
         Node<String> root = new Node<String>("A", lchild, rchild);
 
         Tree<String> tree = new Tree<String>(root);
-        tree.postorder(tree.root);
+        tree.preorder(tree.root);
     }
 }
